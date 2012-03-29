77b6780746c301fa51b790b4e65f4d8a555c0390
nerzhul <nerzhul@92ff3262-0674-4eea-be19-dc134f80368d>
2012-03-29 18:46:11 +0000

    use backend for DHCP and network ip into ipmanager
    git-svn-id: svn://frostsapphirestudios.com/monitor@86 92ff3262-0674-4eea-be19-dc134f80368d

--- modules/user_modules/ipmanager/module.php
+++ modules/user_modules/ipmanager/module.php
@@ -1,5 +1,6 @@
 <?php
 	require_once(dirname(__FILE__)."/../generic_module.php");
+	require_once(dirname(__FILE__)."/../../../lib/FSS/modules/Network.FS.class.php");
 	class iIPManager extends genModule{
 		function iIPManager() { parent::genModule(); }
 		public function Load() {
@@ -12,109 +13,94 @@
 
 		private function showStats() {
 			$output = "";
+			$formoutput = "";
+			$netoutput = "";
 			
+			$resarray = array();
+			// @TODO filter
 			$query = FS::$dbMgr->Select("fss_dhcp_subnet_cache","netid,netmask");
-
-
-			$filter = FS::$secMgr->checkAndSecuriseGetData("f");
-
-			if(count($sort_result) > 0) {
-				$output .= FS::$iMgr->addForm("index.php?mod=".$this->mid."&act=1");
-				$output .= FS::$iMgr->addList("f");
-				foreach ($sort_result as $class_a => $ca_keys) {
-					foreach ($ca_keys as $class_b => $cb_keys) {
-							foreach ($cb_keys as $class_c => $cc_keys) {
-								if(isset($cc_keys["net"])) {
-									$netw = $cc_keys["net"]."/".$cc_keys["mask"];
-									$output .= FS::$iMgr->addElementTolist($netw,$netw,($filter == $cc_keys["net"]."/".$cc_keys["mask"] ? true : false));
-								}
-						}
-					}
+			
+			$output .= FS::$iMgr->addForm("index.php?mod=".$this->mid."&act=1");
+			$output .= FS::$iMgr->addList("f");
+				
+			while($data = mysql_fetch_array($query)) {
+				$iparray = array();
+				$formoutput .= FS::$iMgr->addElementTolist($data["netid"]."/".$data["netmask"],$data["netid"]."/".$data["netmask"],($filter == $data["netid"]."/".$data["netmask"] ? true : false));
+				$netoutput .= "<h4>Réseau : ".$data["netid"]."/".$data["netmask"]."</h4>";
+				$netoutput .= "<center><canvas id=\"".$data["netid"]."\" height=\"300\" width=\"450\">HTML5 non support&eacute;</canvas></center>";
+				$netoutput .= "<center><table><tr><th>Adresse IP</th><th>Statut</th><th>MAC address</th><th>Nom d'hote</th><th>Fin du bail</th></tr>";
+				$netobj = new FSNetwork();
+				$netobj->setNetAddr($data["netid"]);
+				$netobj->setNetMask($data["netmask"]);
+				for($i=ip2long($netobj->getFirstUsableIP());$i<=($netobj->getLastUsableIP());$i++) {
+					$iparray[$i] = array();
+					$iparray[$i]["mac"] = "";
+					$iparray[$i]["host"] = "";
+					$iparray[$i]["ltime"] = "";
+					$iparray[$i]["distrib"] = 0;
 				}
-				$output .= "</select>";
-				$output .= FS::$iMgr->addSubmit("Filtrer","Filtrer");
-				$output .= "</form>";
-				foreach ($sort_result as $class_a => $ca_keys) {
-					foreach ($ca_keys as $class_b => $cb_keys) {
-						if($filter == NULL) $output .= "<h3>Classe B: $class_a.$class_b.0.0/16</h3>";
-						foreach ($cb_keys as $class_c => $cc_keys) {
-							if(isset($cc_keys["mask"]) && ($filter == NULL || $filter == $cc_keys["net"]."/".$cc_keys["mask"])) {
-								$usableaddr = ~(ip2long($cc_keys["mask"]));
-								$usableaddr += 4294967295 - 4;
-								$output .= "<h4>Classe C: ".$cc_keys["net"]."/".$cc_keys["mask"]."</h4>";
-								$output .= "<center><canvas id=\"".$cc_keys["net"]."\" height=\"300\" width=\"450\">HTML5 non support&eacute;</canvas></center>";
-								$usedaddr = (count($cc_keys)-2);
-								$output .= "<center><table><tr><th>Adresse IP</th><th>Statut</th><th>MAC address</th><th>Nom d'hote</th><th>Fin du bail</th></tr>";
-								$used = 0;
-								$reserv = 0;
-								foreach($cc_keys as $ipKey => $ipData) {
-									if($ipKey == "mask" || $ipKey == "net")
-										continue;
-									if(isset($ipData["state"])) $rstate = $ipData["state"];
-									else $rstate = "";
-									switch($rstate) {
-									case "free":
-										$rstate = "Libre";
-										$style = "background-color: #BFFFBF;";
-										break;
-									case "active":
-									case "expired":
-									case "abandoned":
-										$rstate = "Utilis&eacute;";
-										$style = "background-color: #FF6A6A;";
-										$used++;
-										break;
-									case "reserved":
-										$rstate = "R&eacute;serv&eacute;";
-										$style = "background-color: #FFFF80;";
-										$reserv++;
-										break;
-									default:
-										$rstate = "";
-										$style = "";
-										break;
-									}
-	
-									if($rstate) {
-										if(isset($ipData["hw"])) $iwh = $ipData["hw"];
-										else $iwh = "";
-										if(isset($ipData["hostname"])) $ihost = $ipData["hostname"];
-																		else $ihost = "";
-										if(isset($ipData["end"])) $iend = $ipData["end"];
-																		else $iend = "";
-		
-										$output .= "<tr style=\"$style\"><td><a class=\"monoComponent_li_a\" href=\"index.php?mod=33&node=".$class_a.".".$class_b.".".$class_c.".".$ipKey."\">";
-										$output .= $class_a.".".$class_b.".".$class_c.".".$ipKey."</a>";
-										$output .= "</td><td>".$rstate."</td><td>";
-										$output .= "<a class=\"monoComponent_li_a\" href=\"index.php?mod=33&node=".$iwh."\">".$iwh."</a></td><td>";
-										$output .= $ihost."</td><td>";
-										$output .= $iend."</td></tr>";
-									}
-								}
-		
-								$output .= "</table></center><br /><hr>";
-								$free = $usableaddr - $used - $reserv;
-								$output .= "<script>
-								{
-									var pie3 = new RGraph.Pie('".$cc_keys["net"]."', [".$used.",".$reserv.",".$free."]);
-									pie3.Set('chart.key', ['Used (".substr(($used/$usableaddr*100),0,5)."%)', 'Reserved (".substr(($reserv/$usableaddr*100),0,5)."%)', 'Free (".substr(($free/$usableaddr*100),0,5)."%)']);
-									pie3.Set('chart.key.interactive', true);
-									pie3.Set('chart.colors', ['red', 'yellow', 'green']);
-									pie3.Set('chart.shadow', true);
-									pie3.Set('chart.shadow.offsetx', 0);
-									pie3.Set('chart.shadow.offsety', 0);
-									pie3.Set('chart.shadow.blur', 25);
-									pie3.Set('chart.strokestyle', 'white');
-									pie3.Set('chart.linewidth', 3);
-									pie3.Draw();
-								   }</script>";
-							}
-						}
-					}
+				$query2 = FS::$dbMgr->Select("fss_dhcp_ip_cache","ip,macaddr,hostname,leasetime,distributed","netid = '".$data["netid"]."'");
+				while($data2 = mysql_fetch_array($query2)) {
+					$iparray[ip2long($data2["ip"])]["mac"] = $data2["macaddr"];
+					$iparray[ip2long($data2["ip"])]["host"] = $data2["hostname"];
+					$iparray[ip2long($data2["ip"])]["ltime"] = $data2["leasetime"];
+					$iparray[ip2long($data2["ip"])]["distrib"] = $data2["distributed"];
 				}
+				
+				$used = 0;
+				$reserv = 0;
+				foreach($iparray as $key => $value) {
+					$rstate = "";
+					$style = "";
+					switch($value["distrib"]) {
+						case 1:
+							$rstate = "Libre";
+							$style = "background-color: #BFFFBF;";
+							break;
+						case 2:
+							$rstate = "Utilis&eacute;";
+							$style = "background-color: #FF6A6A;";
+							$used++;
+							break;
+						case 3:
+							$rstate = "R&eacute;serv&eacute;";
+							$style = "background-color: #FFFF80;";
+							$reserv++;
+							break;
+						default:
+							$rstate = "";
+							$style = "";
+							break;
+					}
+					$netoutput .= "<tr style=\"$style\"><td><a class=\"monoComponent_li_a\" href=\"index.php?mod=".FS::$iMgr->getModuleIdByPath("switches")."&node=".long2ip($key)."\">";
+					$netoutput .= long2ip($key)."</a>";
+					$netoutput .= "</td><td>".$rstate."</td><td>";
+					$netoutput .= "<a class=\"monoComponent_li_a\" href=\"index.php?mod=".FS::$iMgr->getModuleIdByPath("switches")."&node=".$value["mac"]."\">".$value["mac"]."</a></td><td>";
+					$netoutput .= $value["host"]."</td><td>";
+					$netoutput .= $value["end"]."</td></tr>";
+				}
+				
+				$netoutput .= "</table></center><br /><hr>";
+				$netoutput .= "<script>
+				{
+					var pie3 = new RGraph.Pie('".$cc_keys["net"]."', [".$used.",".$reserv.",".$free."]);
+					pie3.Set('chart.key', ['Used (".substr(($used/$usableaddr*100),0,5)."%)', 'Reserved (".substr(($reserv/$usableaddr*100),0,5)."%)', 'Free (".substr(($free/$usableaddr*100),0,5)."%)']);
+					pie3.Set('chart.key.interactive', true);
+					pie3.Set('chart.colors', ['red', 'yellow', 'green']);
+					pie3.Set('chart.shadow', true);
+					pie3.Set('chart.shadow.offsetx', 0);
+					pie3.Set('chart.shadow.offsety', 0);
+					pie3.Set('chart.shadow.blur', 25);
+					pie3.Set('chart.strokestyle', 'white');
+					pie3.Set('chart.linewidth', 3);
+					pie3.Draw();
+				   }</script>";
 			}
-			else
-				FS::$iMgr->printError("Aucun réseau IP n'a été trouvé dans le(s) serveur(s) DHCP !");
+			$output .= $formoutput;
+			$output .= "</select>";
+			$output .= FS::$iMgr->addSubmit("Filtrer","Filtrer");
+			$output .= "</form>";
+			$output .= $netoutput;
 
 			return $output;
 		}
