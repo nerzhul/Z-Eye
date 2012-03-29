af7dc9cd594b5403f0c2e3d15fd3845c40dc228b
nerzhul <nerzhul@92ff3262-0674-4eea-be19-dc134f80368d>
2012-03-29 19:40:19 +0000

    git-svn-id: svn://frostsapphirestudios.com/monitor@97 92ff3262-0674-4eea-be19-dc134f80368d

--- modules/user_modules/ipmanager/module.php
+++ modules/user_modules/ipmanager/module.php
@@ -86,7 +86,7 @@
 				$netoutput .= "<script>
 				{
 					var pie3 = new RGraph.Pie('".$data["netid"]."', [".$used.",".$reserv.",".$dhcpfree.",".$unkst."]);
-					pie3.Set('chart.key', ['Used (".substr(($used/count($iparray)*100),0,5)."%)', 'Reserved (".substr(($reserv/count($iparray)*100),0,5)."%)', 'DHCPFree (".substr(($free/count($iparray)*100),0,5)."%)', 'Unknown status (".substr(($unkst/count($iparray)*100),0,5)."%)' ]);
+					pie3.Set('chart.key', ['Used (".substr(($used/count($iparray)*100),0,5)."%)', 'Reserved (".substr(($reserv/count($iparray)*100),0,5)."%)', 'DHCPFree (".substr(($dhcpfree/count($iparray)*100),0,5)."%)', 'Unknown status (".substr(($unkst/count($iparray)*100),0,5)."%)' ]);
 					pie3.Set('chart.key.interactive', true);
 					pie3.Set('chart.colors', ['red', 'yellow', 'gray', 'green']);
 					pie3.Set('chart.shadow', true);
