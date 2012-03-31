463c4f172ac300135b900ecb81a31aae73613ccc
nerzhul <nerzhul@92ff3262-0674-4eea-be19-dc134f80368d>
2012-03-31 09:44:53 +0000

    Added servermgmt module to manage the analysis of the servers
    git-svn-id: svn://frostsapphirestudios.com/monitor@114 92ff3262-0674-4eea-be19-dc134f80368d

---
+++ modules/user_modules/servermgmt/module.php
@@ -0,0 +1,34 @@
+<?php
+	require_once(dirname(__FILE__)."/../generic_module.php");
+	require_once(dirname(__FILE__)."/../../../lib/FSS/LDAP.FS.class.php");
+	class iServerMgmt extends genModule{
+		function iServerMgmt() { parent::genModule(); }
+		public function Load() {
+			$output = "<div id=\"module_connect\"><h3>Gestion du moteur d'analyse des serveurs</h3>";
+			$output .= $this->showServerList();
+			$output .= "</div>";
+			return $output;
+		}
+		
+		private function showServerList() {
+			$output = "";
+			$tmpoutput = "<table class=\"standardTable\"><tr><th>Serveur</th><th>Login</th><th>DHCP ?</th><th>DNS</th><th>Supprimer</th></tr>";
+			$found = false;
+			$query = FS::$dbMgr->Select("fss_server_list","addr,login,dhcp,dns");
+			while($data = mysql_fetch_array($query)) {
+				if($found == false) $found = true;
+				$tmpoutput .= "<tr><td>".$data["addr"]."</td><td>".$data["login"]."</td><td>".($data["dhcp"] > 0 ? "X" : "")."</td><td>".($data["dns"] > 0 ? "X" : "")."</td><td></td></tr>";
+			}
+			if($found) {
+				$output .= $tmpoutput."</table>";
+			}
+			return $output;	
+		}
+		
+		public function handlePostDatas($act) {
+			switch($act) {
+				default: break;
+			}
+		}
+	};
+?>
