d03cf71505b55bd13ab99dbd72483533b6ba788f
nerzhul <nerzhul@92ff3262-0674-4eea-be19-dc134f80368d>
2012-04-19 19:50:18 +0000

    typos
    git-svn-id: svn://frostsapphirestudios.com/monitor@225 92ff3262-0674-4eea-be19-dc134f80368d

--- modules/user_modules/search/module.php
+++ modules/user_modules/search/module.php
@@ -67,7 +67,7 @@
 						$found = 1;
 						$tmpoutput .= "<div><h4>VLAN présent dans ces équipements</h4>";
 					}
-					$tmpoutput .= "<li>: <a class=\"monoComponentt_a\" href=\"index.php?mod=".FS::$iMgr->getModuleIdByPath("switches")."&d=".$dname."&fltr=".$search."\">".$dname."</a> (".$data["description"].")<br />";
+					$tmpoutput .= "<li> <a class=\"monoComponentt_a\" href=\"index.php?mod=".FS::$iMgr->getModuleIdByPath("switches")."&d=".$dname."&fltr=".$search."\">".$dname."</a> (".$data["description"].")<br />";
 				}
 			}
 			
@@ -106,9 +106,9 @@
 				}
 				$swname = FS::$pgdbMgr->GetOneData("device","name","ip = '".$data["ip"]."'");
 				$convport = preg_replace("#\/#","-",$data["port"]);
-				$tmpoutput .= "Equipement: <a class=\"monoComponentt_a\" href=\"index.php?mod=".FS::$iMgr->getModuleIdByPath("switches")."&d=".$swname."\">".$swname."</a> (";
-				$tmpoutput .= "<a class=\"monoComponentt_a\" href=\"index.php?mod=".FS::$iMgr->getModuleIdByPath("switches")."&d=".$swname."#".$convport."\">".$data["port"]."</a>";
-				$tmpoutput .= "<a class=\"monoComponentt_a\" href=\"index.php?mod=".FS::$iMgr->getModuleIdByPath("switches")."&d=".$swname."&p=".$data["port"]."\">".FS::$iMgr->addImage("styles/images/pencil.gif",12,12)."</a>) <br />";
+				$tmpoutput .= "Equipement: <a class=\"monoComponentt_a\" href=\"index.php?mod=".FS::$iMgr->getModuleIdByPath("switches")."&d=".$swname."\">".$swname."</a> [";
+				$tmpoutput .= "<a class=\"monoComponentt_a\" href=\"index.php?mod=".FS::$iMgr->getModuleIdByPath("switches")."&d=".$swname."#".$convport."\">".$data["port"]."</a> ";
+				$tmpoutput .= "<a class=\"monoComponentt_a\" href=\"index.php?mod=".FS::$iMgr->getModuleIdByPath("switches")."&d=".$swname."&p=".$data["port"]."\">".FS::$iMgr->addImage("styles/images/pencil.gif",12,12)."</a>]<br />";
 			}
 			if($found) $tmpoutput .= "</div>";
 			$found = 0;
