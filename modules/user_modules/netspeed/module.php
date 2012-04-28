efb17d23da2a984c586df32430d7aa7e2b0ada4a
nerzhul <nerzhul@92ff3262-0674-4eea-be19-dc134f80368d>
2012-04-28 17:34:42 +0000

    prepare weathermap showing by module
    git-svn-id: svn://frostsapphirestudios.com/monitor@245 92ff3262-0674-4eea-be19-dc134f80368d

--- modules/user_modules/netspeed/module.php
+++ modules/user_modules/netspeed/module.php
@@ -4,8 +4,28 @@
 		function iNetSpeed() { parent::genModule(); }
 		public function Load() {
 			$output = "<div id=\"monoComponent\"><h3>Analyse des Débits</h3>";
+			$device = FS::$secMgr->checkAndSecuriseGetData("d");
+			if($device != NULL)
+				$output .= $this->showDeviceWeatherMap($device);
+			else
+				$output .= $this->showGeneralWeatherMap();
 			$output .= "</div>";
 			return $output;
+		}
+		
+		private function showDeviceWeatherMap($device) {
+			$dname = FS::$pgdbMgr->GetOneData("device","name","ip = '".$device."'");
+			if(!$dname)
+				return FS::$iMgr->printError("Cet équipement n'existe pas !");
+				
+			$output = "<h2>Etat des liens de ".$dname."</h2>";
+			$output .= FS::$iMgr->addImage("datas/weathermap/".$dname.".png");
+			return $output;	
+		}
+		private function showGeneralWeatherMap() {
+			$output = "<h2>Etat des liens inter équipements</h2>";
+			$output .= FS::$iMgr->addImage("datas/weathermap/main.png");
+			return $output;	
 		}
 		
 		public function handlePostDatas($act) {
