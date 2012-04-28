5a3ff60b11297d9299d6116671e349c26b6d9226
nerzhul <nerzhul@92ff3262-0674-4eea-be19-dc134f80368d>
2012-04-28 12:48:00 +0000

    bugfix on mrtg_config_discover.php
    git-svn-id: svn://frostsapphirestudios.com/monitor@237 92ff3262-0674-4eea-be19-dc134f80368d

--- scripts/mrtg_config_discover.php
+++ scripts/mrtg_config_discover.php
@@ -3,19 +3,32 @@
 	
 	FS::LoadFSModules();
 	
+	echo "[".Config::getWebsiteName()."][MRTG-Discover] started at ".date('d-m-Y G:i:s')."\n";
+	$start_time = microtime(true);
 	$query = FS::$pgdbMgr->Select("device","ip,name");
 	while($data = pg_fetch_array($query)) {
+		echo "[".Config::getWebsiteName()."][MRTG-Discover] Do: cfgmaker ".SNMPConfig::$SNMPReadCommunity."@".$data["ip"]."\n";
 		$out = "";
 		exec("cfgmaker ".SNMPConfig::$SNMPReadCommunity."@".$data["ip"],$out);
-		$out = preg_replace("#Workdir: (.*)\n#","Workdir: ".dirname(__FILE__)."/../datas/rrd\n",$out);
 		$file = fopen(dirname(__FILE__)."/../datas/mrtg-config/mrtg-".$data["name"].".cfg","w+");
 		if(!$file) {
 			echo "[FATAL] Cannot write file to ".dirname(__FILE__)."/../datas/mrtg-config/ !";
 			exit(1);
 		}
-		fwrite($file,$out);
+		echo "[".Config::getWebsiteName()."][MRTG-Discover] Do: write ".dirname(__FILE__)."/../datas/mrtg-config/mrtg-".$data["name"].".cfg\n";
+		for($i=0;$i<count($out);$i++) {
+			if(preg_match("#WorkDir:#",$out[$i]) && $wkdirwr == 0) {
+				$towr = "WorkDir: ".dirname(__FILE__)."/../datas/rrd\n";
+				$wkdirwr = 1;
+			}
+			else
+				$towr = $out[$i];
+				
+			fwrite($file,$out[$i]."\n");
+		}
 		fclose($file);
 	}
-	
-	echo "[".Config::getWebsiteName()."] MRTG Discover done at ".date('d-m-Y G:i:s')."\n";
+	$end_time = microtime(true);
+	$script_time = $end_time - $start_time;
+	echo "[".Config::getWebsiteName()."][MRTG-Discover] done at ".date('d-m-Y G:i:s')." (Execution time: ".$script_time.")\n";
 ?>
