4243af9a31ea65e2e31e9cef312a64fd36275c56
nerzhul <nerzhul@92ff3262-0674-4eea-be19-dc134f80368d>
2012-04-28 21:05:50 +0000

    style
    git-svn-id: svn://frostsapphirestudios.com/monitor@248 92ff3262-0674-4eea-be19-dc134f80368d

--- scripts/netgraph_generator.php
+++ scripts/netgraph_generator.php
@@ -11,7 +11,7 @@
 		exit(1);
 	}
 	
-	$graphbuffer = "graph maingraph {\ngraph [size=\"30,30\", ratio=fill, bgcolor=white, color=black, fontcolor=white, fontname=lucon, fontsize=14, nodesep=2, overlap=scale, rankrep=\"0.3\", splines=false];\n	node {label=\"\\N\", fillcolor=dimgrey, fixedsize=false, fontcolor=white, fontname=lucon, fontsize=12, shape=box, style=filled];\n edge [color=black};\ngraph [ratio=compress];\n";
+	$graphbuffer = "digraph maingraph {\ngraph [size=\"100,100\", bgcolor=white, nodesep=1];\n	node [label=\"\N\", fillcolor=dimgrey, fixedsize=false, fontcolor=white, fontname=lucon, fontsize=16, shape=plaintext, style=filled];\n edge [color=black};\ngraph [ratio=compress];\n";
 	
 	$nodelist = array();
 	$query = FS::$pgdbMgr->Select("device","name");
@@ -20,31 +20,28 @@
 			$nodelist[count($nodelist)] = $data["name"];
 	}
 	
-	$linklist = array();
 	$query = FS::$pgdbMgr->Select("device_port","remote_id","remote_id != ''");
-	$neighbor_arr = array();
 	while($data = pg_fetch_array($query)) {
-		if(!in_array($data["remote_id"]))
+		if(!in_array($data["remote_id"],$nodelist))
 			$nodelist[count($nodelist)] = $data["remote_id"];
 	}
 	
 	for($i=0;$i<count($nodelist);$i++) {
-		 $graphbuffer .= preg_replace("#[.-]#","_",$nodelist[$i])." [label=\"".$nodelist[$i]."\", URL=\"index.php?mod=".FS::$iMgr->getModuleIdByPath("switches")."&d=".$nodelist[$i]."\"];\n";
+		 $graphbuffer .= preg_replace("#[.-]#","_",$nodelist[$i])." [label=\"".$nodelist[$i]."\", URL=\"index.php?mod=".FS::$iMgr->getModuleIdByPath("switches")."&d=".$nodelist[$i]."\", fontsize=72];\n";
 	}
 	
 	$query = FS::$pgdbMgr->Select("device_port","ip,remote_id","remote_id != ''");
 	while($data = pg_fetch_array($query)) {
-		if(!in_array($data["remote_id"]))
+		if(!in_array($data["remote_id"],$nodelist))
 			$nodelist[count($nodelist)] = $data["remote_id"];
 		$dname = FS::$pgdbMgr->GetOneData("device","name","ip = '".$data["ip"]."'");
 		$graphbuffer .= preg_replace("#[.-]#","_",$dname)." -> ".preg_replace("#[.-]#","_",$data["remote_id"])."\n";
 	}
-	// $graphbuffer .= "node".$ncount." [label=\"".$data["name"]."\", URL=\"index.php?mod=".FS::$iMgr->getModuleIdByPath("switches")."&d=".$data["name"]."\"];\n";
 	
+	$graphbuffer .= "}\n";
 	
-	$graphbuffer = "}\n";
-	
-	echo $graphbuffer;
+	fwrite($file,$graphbuffer);
+	fclose($file);
 	$end_time = microtime(true);
 	$script_time = $end_time - $start_time;
 	echo "[".Config::getWebsiteName()."][NetGraph-Generator] done at ".date('d-m-Y G:i:s')." (Execution time: ".$script_time."s)\n";
