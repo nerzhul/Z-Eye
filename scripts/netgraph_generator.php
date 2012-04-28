3b69e0fba1193dbcd495ccc7c0e5d7e8d2d37312
nerzhul <nerzhul@92ff3262-0674-4eea-be19-dc134f80368d>
2012-04-28 22:16:39 +0000

    create graph with no wifi AP
    git-svn-id: svn://frostsapphirestudios.com/monitor@253 92ff3262-0674-4eea-be19-dc134f80368d

--- scripts/netgraph_generator.php
+++ scripts/netgraph_generator.php
@@ -11,7 +11,7 @@
 		exit(1);
 	}
 	
-	$graphbuffer = "digraph maingraph {\ngraph [bgcolor=white, nodesep=0.5];\n	node [label=\"\N\", color=white, fontcolor=black, fontname=lucon, fontsize=16, shape=plaintext];\n edge [color=black,fontsize=12];\n";
+	$graphbuffer = "digraph maingraph {\ngraph [bgcolor=white, nodesep=1];\n	node [label=\"\N\", color=white, fontcolor=black, fontname=lucon, shape=plaintext];\n edge [color=black];\n";
 	
 	$nodelist = array();
 	$query = FS::$pgdbMgr->Select("device","name");
@@ -27,7 +27,7 @@
 	}
 	
 	for($i=0;$i<count($nodelist);$i++) {
-		 $graphbuffer .= preg_replace("#[.-]#","_",$nodelist[$i])." [label=\"".$nodelist[$i]."\", URL=\"index.php?mod=".FS::$iMgr->getModuleIdByPath("switches")."&d=".$nodelist[$i]."\", fontsize=72];\n";
+		 $graphbuffer .= preg_replace("#[.-]#","_",$nodelist[$i])." [label=\"".$nodelist[$i]."\", URL=\"index.php?mod=".FS::$iMgr->getModuleIdByPath("switches")."&d=".$nodelist[$i]."\"];\n";
 	}
 	
 	$peer_arr = array();
@@ -47,6 +47,56 @@
 	
 	fwrite($file,$graphbuffer);
 	fclose($file);
+	
+	exec("circo -Tsvg ".dirname(__FILE__)."/../datas/weathermap/main.dot -o ".dirname(__FILE__)."/../datas/weathermap/main.svg");
+	exec("circo -Tpng ".dirname(__FILE__)."/../datas/weathermap/main.dot -o ".dirname(__FILE__)."/../datas/weathermap/main.png");
+	
+	// Without WiFi APs
+	$file = fopen(dirname(__FILE__)."/../datas/weathermap/main-nowifi.dot","w+");
+	if(!$file) {
+		echo  "[".Config::getWebsiteName()."][NetGraph-Generator][FATAL] Can't write ".dirname(__FILE__)."/../datas/weathermap/main-nowifi.dot !";
+		exit(1);
+	}
+	
+	$graphbuffer = "digraph maingraph {\ngraph [bgcolor=white, nodesep=1];\n	node [label=\"\N\", color=white, fontcolor=black, fontname=lucon, shape=plaintext];\n edge [color=black];\n";
+	
+	$nodelist = array();
+	$query = FS::$pgdbMgr->Select("device","model,name");
+	while($data = pg_fetch_array($query)) {
+		if(!in_array($data["name"],$nodelist) && !preg_match("#AIRAP#",$data["model"]))
+			$nodelist[count($nodelist)] = $data["name"];
+	}
+	
+	$query = FS::$pgdbMgr->Select("device_port","remote_id","remote_id != ''");
+	while($data = pg_fetch_array($query)) {
+		$dmodel = FS::$pgdbMgr->GetOneData("device","model","ip = '".$data["ip"]."'");
+		if(!in_array($data["remote_id"],$nodelist) && !preg_match("#AIRAP#",$dmodel))
+			$nodelist[count($nodelist)] = $data["remote_id"];
+	}
+	
+	for($i=0;$i<count($nodelist);$i++) {
+		 $graphbuffer .= preg_replace("#[.-]#","_",$nodelist[$i])." [label=\"".$nodelist[$i]."\", URL=\"index.php?mod=".FS::$iMgr->getModuleIdByPath("switches")."&d=".$nodelist[$i]."\"];\n";
+	}
+	
+	$peer_arr = array();
+	$query = FS::$pgdbMgr->Select("device_port","ip,remote_id","remote_id != ''");
+	while($data = pg_fetch_array($query)) {
+		if(!in_array($data["remote_id"],$nodelist))
+			$nodelist[count($nodelist)] = $data["remote_id"];
+		$dname = FS::$pgdbMgr->GetOneData("device","name","ip = '".$data["ip"]."'");
+		if(!isset($peer_arr[$data["ip"]])) $peer_arr[$data["ip"]] = array();
+		if(!in_array($data["remote_id"],$peer_arr[$data["ip"]])) {
+			$peer_arr[$data["ip"]][count($peer_arr[$data["ip"]])] = $data["remote_id"];
+			$graphbuffer .= preg_replace("#[.-]#","_",$dname)." -> ".preg_replace("#[.-]#","_",$data["remote_id"])."\n";
+		}
+	}
+	
+	$graphbuffer .= "}\n";
+	
+	fwrite($file,$graphbuffer);
+	fclose($file);
+	exec("circo -Tsvg ".dirname(__FILE__)."/../datas/weathermap/main-nowifi.dot -o ".dirname(__FILE__)."/../datas/weathermap/main-nowifi.svg");
+	exec("circo -Tpng ".dirname(__FILE__)."/../datas/weathermap/main-nowifi.dot -o ".dirname(__FILE__)."/../datas/weathermap/main-nowifi.png");
 	$end_time = microtime(true);
 	$script_time = $end_time - $start_time;
 	echo "[".Config::getWebsiteName()."][NetGraph-Generator] done at ".date('d-m-Y G:i:s')." (Execution time: ".$script_time."s)\n";
