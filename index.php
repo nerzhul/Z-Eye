41d7afc779ecd18a279a2f55b45385f782b91957
nerzhul <nerzhul@92ff3262-0674-4eea-be19-dc134f80368d>
2012-04-29 12:39:07 +0000

    include jQueryUI
    git-svn-id: svn://frostsapphirestudios.com/monitor@263 92ff3262-0674-4eea-be19-dc134f80368d

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
 		FS::$sessMgr->InitSessionIfNot();
 		FS::$iMgr->addStylesheet("styles/fss1.css");
 		FS::$iMgr->addJSFile("lib/jQuery/jQuery.js");
+		FS::$iMgr->addJSFile("lib/jQuery/jQueryUI.js");
 		FS::$iMgr->addJSFile("lib/jQuery/jQuery-zoom.js");
 		FS::$iMgr->addJSFile("lib/jQuery/jQuery.imageLens.js");
 		FS::$iMgr->addJSFile("modules/js/canvas.js");
