68f5359ccb2f8bab0fa927d71342b6b3cea1a28a
nerzhul <nerzhul@92ff3262-0674-4eea-be19-dc134f80368d>
2012-05-02 18:54:15 +0000

    git-svn-id: svn://frostsapphirestudios.com/monitor@301 92ff3262-0674-4eea-be19-dc134f80368d

--- modules/user_modules/netspeed/module.php
+++ modules/user_modules/netspeed/module.php
@@ -32,10 +32,11 @@
 			return $output;	
 		}
 		private function showGeneralLightWeatherMap() {
-			$output = "<h2>Carte du réseau</h2><div id=\"netmapdL\" style=\"width: 1000px; height: 700px; overflow: hidden;\"><div style=\"background: url(datas/weathermap/main-nowifi.svg) no-repeat; width: 1000px; height: 700px;\">";
+			$output = "<h2>Carte du réseau</h2><div id=\"netmapdL\" style=\"width: 1000px; height: 700px; overflow: hidden;\"><div style=\"background: url(datas/weathermap/main-nowifi.svg); no-repeat; width: 1000px; height: 700px;\">";
 			$output .= FS::$iMgr->addImage("datas/weathermap/main-nowifi.svg",1000,700,"netmapL")."</div>";
-			list($imgw,$imgh,$imgt,$imgattr) = getimagesize("datas/weathermap/main-nowifi.svg");
-			$output .= "<div style=\"width:".$imgw."px; height:".$imgh."px;\">";
+			$imgsize = getimagesize("datas/weathermap/main-nowifi.svg");
+			$sizes = preg_split("#\"#",$imgsize[3]);
+			$output .= "<div style=\"width:".$sizes[1]."px; height:".$sizes[3]."px;\">";
         	$output .= FS::$iMgr->addImage("datas/weathermap/main-nowifi.svg",1000,700,"netmapL");
         	$output .= "<div class=\"mapcontent\">";
 			$output .= "</div></div></div><script type=\"text/javascript\">$('#netmapdL').mapbox({mousewheel: true});</script>";
