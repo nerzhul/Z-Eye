c3b272248796301f0cc639fae753913e1a8996ee
nerzhul <nerzhul@92ff3262-0674-4eea-be19-dc134f80368d>
2012-05-16 17:56:15 +0000

    graph is now ok
    git-svn-id: svn://frostsapphirestudios.com/monitor@376 92ff3262-0674-4eea-be19-dc134f80368d

--- modules/user_modules/securityreport/module.php
+++ modules/user_modules/securityreport/module.php
@@ -71,14 +71,9 @@
 					$output .= FS::$iMgr->addHidden("mod",$this->mid);
 					$output .= "Pas: ".FS::$iMgr->addNumericInput("ech",$ech,2,2)." jours <br />";
 					$output .= "Echelle: ".FS::$iMgr->addNumericInput("ec",$ec,3,3)." jours <br />";
-					/*$output .= "Filtres: ";
-					$output .= FS::$iMgr->addCheck("sc",$shscan)."Scans ";
-					$output .= FS::$iMgr->addCheck("tse",$shtse)."TSE ";
-					$output .= FS::$iMgr->addCheck("ssh",$shssh)."SSH ";*/
 		
 					$output .= FS::$iMgr->addSubmit("Mise à jour","Mise à jour")."<br />";
 					$output .= "</form>";
-					//$output .= "<canvas id=\"atkst\" height=\"450\" width=\"1175\"></canvas>";
 					$output .= "<div id=\"atkst\"></div>";
 					$year = date("Y");
 					$month = date("m");
@@ -87,40 +82,37 @@
 					$sql_date = $year."-".$month."-".$day." 00:00:00";
 					$fields = "";
 					
-					/*if($shscan) */$fields .= ",scans";
-					/*if($shtse) */$fields .= ",tse";
-					/*if($shssh) */$fields .= ",ssh";
+					$fields .= ",scans";
+					$fields .= ",tse";
+					$fields .= ",ssh";
 					
-					$sql = "select atkdate".$fields." from attack_stats where atkdate > (SELECT DATE_SUB('".$sql_date."', INTERVAL ".$ec." DAY))";
+					$sql = "select atkdate".$fields." from attack_stats where atkdate > (SELECT DATE_SUB('".$sql_date."', INTERVAL ".($ec+15)." DAY))";
 					$query = mysql_query($sql);
 					$labels = $scans = $tse = $ssh = "[";
 					$cursor = 0;
-					$subline = false;
-					$temp1 = $temp2 = $temp3 = $temp4 = "";
+					$temp1 = $temp2 = $temp3 = $temp4 = 0;
 					while($data = mysql_fetch_array($query)) {
-							if($cursor == $ech) {
-									if($subline)
-											$labels .= "'\\r\\n".$temp1."',";
-									else
-											$labels .= "'".$temp1."',";
-									/*if($shscan) */$scans .= $temp2.",";
-									/*if($shtse) */$tse .= $temp3.",";
-									/*if($shssh) */$ssh .= $temp4.",";
-									$cursor = $temp1 = $temp2 = $temp3 = $temp4 = 0;
-									$subline = ($subline ? false : true);
-							} else {
+							if($cursor != $ech || $ech == 1) {
 									$cursor++;
 									$temp1 = substr($data["atkdate"],8,2)."/".substr($data["atkdate"],5,2);
-									/*if($shscan) */$temp2 += $data["scans"];
-									/*if($shtse) */$temp3 += $data["tse"];
-									/*if($shssh) */$temp4 += $data["ssh"];
+									$temp2 += $data["scans"];
+									$temp3 += $data["tse"];
+									$temp4 += $data["ssh"];
+							}
+							
+							if($cursor == $ech) {
+									$labels .= "'".$temp1."',";
+									$scans .= $temp2.",";
+									$tse .= $temp3.",";
+									$ssh .= $temp4.",";
+									$cursor = $temp1 = $temp2 = $temp3 = $temp4 = 0;
 							}
 					}
 		
 					$labels .= "]";
-					/*if($shscan) */$scans .= "]";
-					/*if($shtse) */$tse .= "]";
-					/*if($shssh) */$ssh .= "]";
+					$scans .= "]";
+					$tse .= "]";
+					$ssh .= "]";
 					
 					$output .= "<script type=\"text/javascript\">(function($){ var hchart;
 							hchart = new Highcharts.Chart({
@@ -136,42 +128,6 @@
 							});
 						})(jQuery);
 						</script>";
-					/*$output .= "<script type=\"text/javascript\">var data = ";
-		
-					$output .= "[";
-					if($shscan) $output .= $scans;
-					if($shtse) {
-						if($shscan) $output .= ",";
-						$output .= $tse;
-					}
-					if($shssh) {
-						if($shscan || $shtse) $output .= ",";
-						$output .= $ssh;
-					}
-					$output .= "];";
-		
-					$output .= "var line = new RGraph.Line(\"atkst\", data);";
-					$output .= "line.Set('chart.yaxispos', 'right');
-					line.Set('chart.hmargin', 15);
-					line.Set('chart.tickmarks', 'endcircle');
-					line.Set('chart.linewidth', 2);
-					line.Set('chart.shadow', true);
-					line.Set('chart.gutter.top', 5);
-					line.Set('chart.gutter.right', 100);
-					line.Set('chart.key', [";
-					if($shscan) $output .= "'Scans'";
-					if($shtse) {
-						if($shscan) $output .= ",";
-						$output .= "'Attaques TSE'";
-					}
-					if($shssh) {
-						if($shscan || $shtse) $output .= ",";
-						$output .= "'Attaques SSH'";
-					}
-					$output .= "]);
-					line.Set('chart.gutter.bottom', 45); ";
-					$output .= "line.Set('chart.labels', ".$labels.");";
-					$output .= "line.Draw();</script>";*/
 					$output .= "</body></html>";
 					mysql_select_db("fssmanager");
 				}
@@ -274,13 +230,7 @@
 				case 1:
 					$ech = FS::$secMgr->checkAndSecurisePostData("ech");
 					$ec = FS::$secMgr->checkAndSecurisePostData("ec");
-					/*$sc = FS::$secMgr->checkAndSecurisePostData("sc");
-					($sc != NULL && $sc == "on") ? $sc = 1 : $sc = 0;
-					$tse = FS::$secMgr->checkAndSecurisePostData("tse");
-					($tse != NULL && $tse == "on") ? $tse = 1 : $tse = 0;
-					$ssh = FS::$secMgr->checkAndSecurisePostData("ssh");
-					($ssh != NULL && $ssh == "on") ? $ssh = 1 : $ssh = 0;*/
-					header("Location: index.php?mod=".$this->mid."&sh=1&ech=".$ech."&ec=".$ec/*."&ssh=".$ssh."&tse=".$tse."&sc=".$sc.*/"");
+					header("Location: index.php?mod=".$this->mid."&sh=1&ech=".$ech."&ec=".$ec);
 					break;
 				case 2: case 3: case 4:
 					$topmax = FS::$secMgr->checkAndSecurisePostData("max");
