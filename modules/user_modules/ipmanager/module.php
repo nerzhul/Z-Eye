67cc51167bca3f7a922245839c9987d6ba5445b0
nerzhul <nerzhul@92ff3262-0674-4eea-be19-dc134f80368d>
2012-05-24 20:28:19 +0000

    Implement searching to find if a reservation in DHCP is obsolete. Fix also a jQueryUI bug for A links
    git-svn-id: svn://frostsapphirestudios.com/monitor@394 92ff3262-0674-4eea-be19-dc134f80368d

--- modules/user_modules/ipmanager/module.php
+++ modules/user_modules/ipmanager/module.php
@@ -5,7 +5,7 @@
 		function iIPManager() { parent::genModule(); }
 		public function Load() {
 			$output = "";
-			$output .= "<div id=\"module_connect\"><h3>Supervision IP</h3>";
+			$output .= "<div id=\"module_connect\">";
 			$output .= $this->showStats();
 			$output .= "</div>";
 			return $output;
@@ -17,14 +17,31 @@
 			$netoutput = "";
 
 			$filter = FS::$secMgr->checkAndSecuriseGetData("f");
-			$output .= FS::$iMgr->addForm("index.php?mod=".$this->mid."&act=1");
-			$output .= FS::$iMgr->addList("f");
-			$query = FS::$dbMgr->Select("fss_dhcp_subnet_cache","netid,netmask");
-			while($data = mysql_fetch_array($query)) {
-				$formoutput .= FS::$iMgr->addElementTolist($data["netid"]."/".$data["netmask"],$data["netid"],($filter == $data["netid"] ? true : false));
-			}
-			
-			$query = FS::$dbMgr->Select("fss_dhcp_subnet_cache","netid,netmask",($filter != NULL ? "netid = '".$filter."'" : ""));
+			$showmodule = FS::$secMgr->checkAndSecuriseGetData("sh");
+			if(!FS::isAjaxCall()) {
+				$output .= "<h3>Supervision IP</h3>";
+				$output .= FS::$iMgr->addForm("index.php?mod=".$this->mid."&act=1");
+				$output .= FS::$iMgr->addList("f");
+				$query = FS::$dbMgr->Select("fss_dhcp_subnet_cache","netid,netmask");
+				while($data = mysql_fetch_array($query)) {
+					$formoutput .= FS::$iMgr->addElementTolist($data["netid"]."/".$data["netmask"],$data["netid"],($filter == $data["netid"] ? true : false));
+				}
+				$output .= $formoutput;
+	                        $output .= "</select>";
+                        	$output .= FS::$iMgr->addSubmit("Filtrer","Filtrer");
+                	        $output .= "</form><br />";
+				if(!$filter)
+					return "Veuillez choisir le réseau IP à monitorer: <br /><br />".$output;
+
+				$output .= "<div id=\"contenttabs\"><ul>";
+        	                $output .= "<li><a href=\"index.php?mod=".$this->mid."&at=2&f=".$filter."\">Statistiques</a>";
+                	        $output .= "<li><a href=\"index.php?mod=".$this->mid."&at=2&f=".$filter."&sh=2\">Outils avancés</a>";
+                        	$output .= "</ul></div>";
+	                        $output .= "<script type=\"text/javascript\">$('#contenttabs').tabs({ajaxOptions: { error: function(xhr,status,index,anchor) {";
+        	                $output .= "$(anchor.hash).html(\"Unable to load tab, link may be wrong or page unavailable\");}}});</script>";
+			} else {
+			if(!$showmodule || $showmodule == 1) {
+			$query = FS::$dbMgr->Select("fss_dhcp_subnet_cache","netid,netmask","netid = '".$filter."'");
 			while($data = mysql_fetch_array($query)) {
 				$iparray = array();
 				$netoutput .= "<h4>Réseau : ".$data["netid"]."/".$data["netmask"]."</h4>";
@@ -116,12 +133,21 @@
 							{ name: 'Libres', y:".$free.", color: 'green'}]
 						}]});</script>";
 			}
-			$output .= $formoutput;
-			$output .= "</select>";
-			$output .= FS::$iMgr->addSubmit("Filtrer","Filtrer");
-			$output .= "</form>";
 			$output .= $netoutput;
-
+			} else if($showmodule == 2) {
+				$output .= "<h4>Recherche de réservation obsolète</h4>";
+				$output .= "<script type=\"text/javascript\">function searchobsolete() {";
+				$output .= "$('#obsres').html('".FS::$iMgr->addImage('styles/images/loader.gif')."');";
+				$output .= "$.post('index.php?at=3&mod=".$this->mid."&act=2', { ival: document.getElementsByName('ival')[0].value, obsdata: document.getElementsByName('obsdata')[0].value}, function(data) {";
+                                $output .= "$('#obsres').html(data);";
+                                $output .= "});return false;}</script>";
+				$output .= FS::$iMgr->addForm("index.php?mod=".$this->mid."&act=2");
+				$output .= FS::$iMgr->addHidden("obsdata",$filter);
+				$output .= "Intervalle (jours) ".FS::$iMgr->addNumericInput("ival")."<br />";
+				$output .= FS::$iMgr->addJSSubmit("search","Rechercher","return searchobsolete();");
+				$output .= "</form><div id=\"obsres\"></div>";
+			}
+			}
 			return $output;
 		}
 		
@@ -131,6 +157,30 @@
 					$filtr = FS::$secMgr->checkAndSecurisePostData("f");
 					if($filtr == NULL) header("Location: index.php?mod".$this->mid."");
 					else header("Location: index.php?mod=".$this->mid."&f=".$filtr);
+					return;
+				case 2:
+					$filter = FS::$secMgr->checkAndSecurisePostData("obsdata");
+					$interval = FS::$secMgr->checkAndSecurisePostData("ival");
+					if(!$filter || !FS::$secMgr->isIP($filter) || !$interval || !FS::$secMgr->isNumeric($interval) ||
+						$interval < 1) {
+						echo FS::$iMgr->printError("Requête invalide !");
+						return;
+					}
+
+					$found = false;
+					$output = "";
+	                                $query = FS::$dbMgr->Select("fss_dhcp_ip_cache","ip,macaddr,hostname","netid = '".$filter."' AND distributed = 3");
+	                                while($data = mysql_fetch_array($query)) {
+						$query2 = FS::$pgdbMgr->Select("node_ip","mac,time_last","ip = '".$data["ip"]."' AND time_last < NOW() - INTERVAL '".$interval." day'","time_last",1);
+			                        while($data2 = pg_fetch_array($query2)) {
+							if($data2["mac"] == $data["macaddr"]) {
+								if(!$found) $found = true;
+								$output .= $data["ip"]." / <a class=\"monoComponentt_a\" href=\"index.php?mod=".FS::$iMgr->getModuleIdByPath("search")."&s=".$data2["mac"]."\">".$data2["mac"]."</a><br />";
+							}
+						}
+					}
+					if($found) echo "<h4>Réservations obsolètes trouvées !</h4>".$output;
+					else echo FS::$iMgr->printDebug("Aucune réservation obsolète trouvée");
 					return;
 			}
 		}
