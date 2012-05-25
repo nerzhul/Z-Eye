23cc96024188a58f772ad45ae05d175e588d2c6f
Ner'zhul <nerzhultheking@gmail.com>
2012-05-25 21:02:17 +0200

    remove full rgraph support

--- lib/FSS/MySQLMgr.FS.class.php
+++ lib/FSS/MySQLMgr.FS.class.php
@@ -1,8 +1,22 @@
 <?php
+	/*
+	* Copyright (C) 2007-2012 Frost Sapphire Studios <http://www.frostsapphirestudios.com/>
+	*
+	* This program is free software; you can redistribute it and/or modify
+	* it under the terms of the GNU General Public License as published by
+	* the Free Software Foundation; either version 2 of the License, or
+	* (at your option) any later version.
+	*
+	* This program is distributed in the hope that it will be useful,
+	* but WITHOUT ANY WARRANTY; without even the implied warranty of
+	* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	* GNU General Public License for more details.
+	*
+	* You should have received a copy of the GNU General Public License
+	* along with this program; if not, write to the Free Software
+	* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+	*/
 	
-	/** This code is Property of Frost Sapphire Studios, all rights reserved.
-	*	All modification is stricty forbidden without Frost Sapphire Studios Agreement
-	**/
 	require_once(dirname(__FILE__)."/../../config/db.conf.php");
 	require_once(dirname(__FILE__)."/../../config/global.conf.php");
 	require_once(dirname(__FILE__)."/../../config/".Config::getSysLang().".lang.php");
--- modules/user_modules/sandbox/module.php
+++ modules/user_modules/sandbox/module.php
@@ -1,21 +1,10 @@
 <?php
 require_once(dirname(__FILE__)."/../generic_module.php");
-include_once(dirname(__FILE__)."/../../../lib/FSS/modules/RGraph.FS.class.php");
 	class iSandBox extends genModule{
 		function iSandBox() { parent::genModule(); }
 		
 		public function Load() {
 			$output = "";
-			$output .= FS::$iMgr->addCanvas("testcanvas");
-			$graph = new FSRGraph();
-			$graph->setType(1);
-			$graph->setName("testcanvas");
-			$graph->setProperty("background.barcolor1","'#000'");
-			$graph->setProperty("shadow","true");
-			$graph->setProperty("shadow.blur","15");
-			$graph->setValues("1,6,9,8,36,54,4,8,6,5,7,5,2,3");
-			// ALl properties
-			$output .= $graph->create();
 			return $output;
 		}
 	};
