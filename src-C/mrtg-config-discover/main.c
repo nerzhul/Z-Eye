4f178c5aa4d97942c4998c0a8e3237c3853ea606
Ner'zhul <loic.blot@frostsapphirestudios.com>
2012-06-20 11:16:46 +0200

    prepare pgsql support

--- src-C/mrtg-config-discover/main.c
+++ src-C/mrtg-config-discover/main.c
@@ -16,10 +16,65 @@
 	* along with this program; if not, write to the Free Software
 	* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 	*/
+#include "mysqlmgmt.h"
+#include "stdmgmt.h"
+#include <time.h>
 
 int main(int argc, const char** argv)
 {
+	printSystem("[Z-Eye] mrtg_config_discover with %s",mysql_get_client_info());
 	
+	time_t starttime = time(0);
+    struct tm now = *localtime(&starttime);
 
+	printSystem("[Z-Eye] mrtg_config_discover started at %s",asctime(&now));
+
+	MYSQL *conn = mysql_init(NULL);
+	if(conn == NULL)
+	{
+		printError("[Z-Eye][FATAL] mrtg_config_discover can't init MySQL !");
+		return 1;
+	}
+	
+	mysql_host = "localhost";
+	mysql_dbname = "fssmanager";
+	mysql_user = "root";
+	mysql_password = "root";
+	
+	if(mysql_real_connect(conn, mysql_host, mysql_user, mysql_password, mysql_dbname, 0, NULL, 0) == NULL)
+	{
+		printError("[Z-Eye][ERROR] Unable to connect to MySQL server (host %s, database %s, user %s, password %s)",
+			mysql_host,mysql_dbname,mysql_user,mysql_password);
+		return 2;
+	}
+	
+	// pgsqlquery
+	// while 
+	{
+		mysql_query(conn, "SELECT snmpro FROM fss_snmp_cache where device = 'NAME'");
+		MYSQL_RES* result = mysql_store_result(conn);
+		int num_fields = mysql_num_fields(result);
+		char* community;
+		MYSQL_ROW row;
+		if((row = mysql_fetch_row(result)))
+		{
+			community = (char*)malloc(strlen(row[0])*sizeof(char)+1);
+			strcpy(community,row[0]);
+		}
+		
+		mysql_free_result(result);
+		/*if(strlen(community) == 0)
+			continue;*/
+			
+		// create thread
+		
+	}
+	
+	mysql_close(conn);
+	
+	time_t endtime = time(0);
+    now = *localtime(&endtime);
+    time_t interval = endtime - starttime;
+	printSystem("[Z-Eye] mrtg_config_discover ended at %s (Exec time %ds)",asctime(&now),interval);
 	return 0;
 }
