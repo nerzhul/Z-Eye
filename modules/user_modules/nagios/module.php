b08c5b217deb093aefe721d060cd40388f859be2
Ner'zhul <loic.blot@frostsapphirestudios.com>
2012-06-22 09:07:09 +0200

    prepare SQL backend for ICINGA

--- modules/user_modules/nagios/module.php
+++ modules/user_modules/nagios/module.php
@@ -66,31 +66,49 @@
 		private function showMain() {
 			$output = "<h4>Gestion des objets</h4>";
 			
+			$stupdateint = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'STATUS_UPDATE_INT'");
+			$cmdcheckint = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'CMD_CHECK_INT'");
+			$resfreq = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'RESULT_UPDATE_INT'");
+			$maxresultage = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'RESULT_MAX_AGE'");
+			$cachehostlifetime = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'HOST_CACHE_LIFETIME'");
+			$cachesrvlifetime = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'SERVICE_CACHE_LIFETIME'");
+			$srvchecktimeout = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'SRV_STATE_TIMEOUT'");
+			$hostchecktimeout = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'HOST_STATE_TIMEOUT'");
+			$notiftimeout = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'NOTIF_TIMEOUT'");
+			$sensorint = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'SENSOR_TIME'");
+			$lognotif = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'LOG_NOTIF'");
+			$logsrvretries = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'LOG_SRV_RETRIES'");
+			$loghostretries = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'LOG_HOST_RETRIES'");
+			$notifenable = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'ENABLE_NOTIF'");
+			$flapenable = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'ENABLE_FLAG'");
+			$adminmail = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'ADMIN_MAIL'");
+			
+			
 			$output .= "<hr><h4>Configuration globale</h4>";
 			$output .= FS::$iMgr->addForm("index.php?mod=".$this->mid."&act=1");
 			$output .= "<table class=\"standardTable\"><tr><th colspan=\"2\">Délais</th></tr>";
-			$output .= FS::$iMgr->addIndexedLine("Intervalle de mise à jour des status","stupdateint",30);
-			$output .= FS::$iMgr->addIndexedLine("Intervalle de vérification des commandes externes","cmdcheckint",15);
-			$output .= FS::$iMgr->addIndexedLine("Intervalle de mise à jour des résultats des hôtes et services","resfreq",10);
-			$output .= FS::$iMgr->addIndexedLine("Age maximal des résultats","maxresultage",3600);
-			$output .= FS::$iMgr->addIndexedLine("Age maximal de l'état d'un hôte","cachehostlifetime",15);
-			$output .= FS::$iMgr->addIndexedLine("Age maximal de l'état d'un service","cachesrvlifetime",15);
-			$output .= FS::$iMgr->addIndexedLine("Temps maximal de recherche d'état d'un service","srvchecktimeout",60);
-			$output .= FS::$iMgr->addIndexedLine("Temps maximal de recherche d'état d'un hôte","hostchecktimeout",30);
-			$output .= FS::$iMgr->addIndexedLine("Temps maximal d'envoi d'une notification","notiftimeout",30);
-			$output .= FS::$iMgr->addIndexedLine("Intervalle par défaut d'utilisation d'une sonde","interval",60);
+			$output .= FS::$iMgr->addIndexedLine("Intervalle de mise à jour des status","stupdateint",$stupdateint);
+			$output .= FS::$iMgr->addIndexedLine("Intervalle de vérification des commandes externes","cmdcheckint",$cmdcheckint);
+			$output .= FS::$iMgr->addIndexedLine("Intervalle de mise à jour des résultats des hôtes et services","resfreq",$resfreq);
+			$output .= FS::$iMgr->addIndexedLine("Age maximal des résultats","maxresultage",$maxresultage);
+			$output .= FS::$iMgr->addIndexedLine("Age maximal de l'état d'un hôte","cachehostlifetime",$cachehostlifetime);
+			$output .= FS::$iMgr->addIndexedLine("Age maximal de l'état d'un service","cachesrvlifetime",$cachesrvlifetime);
+			$output .= FS::$iMgr->addIndexedLine("Temps maximal de recherche d'état d'un service","srvchecktimeout",$srvchecktimeout);
+			$output .= FS::$iMgr->addIndexedLine("Temps maximal de recherche d'état d'un hôte","hostchecktimeout",$hostchecktimeout);
+			$output .= FS::$iMgr->addIndexedLine("Temps maximal d'envoi d'une notification","notiftimeout",$notiftimeout);
+			$output .= FS::$iMgr->addIndexedLine("Intervalle par défaut d'utilisation d'une sonde","sensorint",$sensorint);
 			$output .= "<tr><th colspan=\"2\">Logs</th></tr>";
 			// Log rotation method
-			$output .= FS::$iMgr->addIndexedCheckLine("Log des notifications","lognotif",true);
-			$output .= FS::$iMgr->addIndexedCheckLine("Log des tentatives sur les services","logsrvretries",true);
-			$output .= FS::$iMgr->addIndexedCheckLine("Log des tentatives sur les hôtes","loghostretries",true);
+			$output .= FS::$iMgr->addIndexedCheckLine("Log des notifications","lognotif",$lognotif);
+			$output .= FS::$iMgr->addIndexedCheckLine("Log des tentatives sur les services","logsrvretries",$logsrvretries);
+			$output .= FS::$iMgr->addIndexedCheckLine("Log des tentatives sur les hôtes","loghostretries",$loghostretries);
 			$output .= "<tr><th colspan=\"2\">Notifications</th></tr>";
-			$output .= FS::$iMgr->addIndexedCheckLine("Activer","notifenable",true);
+			$output .= FS::$iMgr->addIndexedCheckLine("Activer","notifenable",$notifenable);
 			// service_check_timeout_state
 			$output .= "<tr><th colspan=\"2\">Autres</th></tr>";
-			$output .= FS::$iMgr->addIndexedCheckLine("Détection des services instables","flapenable",true);
+			$output .= FS::$iMgr->addIndexedCheckLine("Détection des services instables","flapenable",$flapenable);
 			// date_format
-			$output .= FS::$iMgr->addIndexedCheckLine("Mail de l'administrateur","adminemail","admin@localhost");
+			$output .= FS::$iMgr->addIndexedCheckLine("Mail de l'administrateur","adminmail",$adminmail);
 			$output .= "<tr><th colspan=\"2\">".FS::$iMgr->addSubmit("Enregistrer","Enregistrer")."</th></tr>";
 			$output .= "</table></form>";
 			return $output;	
@@ -106,6 +124,23 @@
 			if($file == NULL || $file == false)
 				return 1;
 				
+			$stupdateint = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'STATUS_UPDATE_INT'");
+			$cmdcheckint = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'CMD_CHECK_INT'");
+			$resfreq = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'RESULT_UPDATE_INT'");
+			$maxresultage = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'RESULT_MAX_AGE'");
+			$cachehostlifetime = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'HOST_CACHE_LIFETIME'");
+			$cachesrvlifetime = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'SERVICE_CACHE_LIFETIME'");
+			$srvchecktimeout = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'SRV_STATE_TIMEOUT'");
+			$hostchecktimeout = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'HOST_STATE_TIMEOUT'");
+			$notiftimeout = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'NOTIF_TIMEOUT'");
+			//$sensorint = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'SENSOR_TIME'");
+			$lognotif = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'LOG_NOTIF'");
+			$logsrvretries = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'LOG_SRV_RETRIES'");
+			$loghostretries = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'LOG_HOST_RETRIES'");
+			$notifenable = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'ENABLE_NOTIF'");
+			$flapenable = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'ENABLE_FLAG'");
+			$adminmail = FS::$dbMgr->GetOneData("fss_icinga_main","icingavalue","icingakey = 'ADMIN_MAIL'");
+				
 			fwrite($file,"# ---- Logs ----\n");
 			fwrite($file,"log_file=/var/log/icinga.log\n");
 			fwrite($file,"# ---- Configuration directory ----\n");
@@ -117,11 +152,11 @@
 			fwrite($file,"precached_object_file=/var/spool/icinga/objects.precache\n");
 			fwrite($file,"resource_file=/usr/local/etc/icinga/resource.cfg\n");
 			fwrite($file,"status_file=/var/spool/icinga/status.dat\n");
-			//fwrite($file,"status_update_interval=".$stupdateint."\n");
+			fwrite($file,"status_update_interval=".$stupdateint."\n");
 			fwrite($file,"icinga_user=icinga\n");
 			fwrite($file,"icinga_group=icinga\n");
 			fwrite($file,"check_external_commands=1\n");
-			//fwrite($file,"command_check_interval=".$cmdcheckint."s\n");
+			fwrite($file,"command_check_interval=".$cmdcheckint."s\n");
 			fwrite($file,"command_file=/var/spool/icinga/rw/icinga.cmd\n");
 			fwrite($file,"external_command_buffer_slots=10240\n");
 			fwrite($file,"lock_file=/var/spool/icinga/icinga.lock\n");
@@ -134,9 +169,9 @@
 			fwrite($file,"use_syslog=1\n");
 			fwrite($file,"use_syslog_local_facility=0\n");
 			fwrite($file,"syslog_local_facility=5\n");
-			//fwrite($file,"log_notifications=".$lognotif."\n");
-			//fwrite($file,"log_service_retries=".$logsrvretries."\n");
-			//fwrite($file,"log_host_retries=".$loghretries."\n");
+			fwrite($file,"log_notifications=".$lognotif."\n");
+			fwrite($file,"log_service_retries=".$logsrvretries."\n");
+			fwrite($file,"log_host_retries=".$loghretries."\n");
 			fwrite($file,"log_event_handlers=1\n");
 			fwrite($file,"log_initial_states=0\n");
 			fwrite($file,"log_current_states=1\n");
@@ -148,12 +183,12 @@
 			fwrite($file,"host_inter_check_delay_method=s\n");
 			fwrite($file,"max_host_check_spread=30\n");
 			fwrite($file,"max_concurrent_checks=0\n");
-			//fwrite($file,"check_result_reaper_frequency=".$resfreq."\n");
+			fwrite($file,"check_result_reaper_frequency=".$resfreq."\n");
 			fwrite($file,"max_check_result_reaper_time=30\n");
 			fwrite($file,"check_result_path=/var/spool/icinga/checkresults\n");
-			//fwrite($file,"max_check_result_file_age=".$maxresultage."\n");
-			//fwrite($file,"cached_host_check_horizon=".$cachehostlifetime."\n");
-			//fwrite($file,"cached_service_check_horizon=".$cachesrvlifetime."\n");
+			fwrite($file,"max_check_result_file_age=".$maxresultage."\n");
+			fwrite($file,"cached_host_check_horizon=".$cachehostlifetime."\n");
+			fwrite($file,"cached_service_check_horizon=".$cachesrvlifetime."\n");
 			fwrite($file,"enable_predictive_host_dependency_checks=1\n");
 			fwrite($file,"enable_predictive_service_dependency_checks=1\n");
 			fwrite($file,"soft_state_dependencies=0\n");
@@ -163,9 +198,9 @@
 			fwrite($file,"auto_rescheduling_window=180\n");
 			fwrite($file,"sleep_time=0.25\n");
 			fwrite($file,"# ---- Timeouts ----\n");
-			//fwrite($file,"service_check_timeout=".$srvchecktimeout."\n");
-			//fwrite($file,"host_check_timeout=".$hostchecktimeout."\n");
-			//fwrite($file,"notification_timeout=".$notiftimeout."\n");
+			fwrite($file,"service_check_timeout=".$srvchecktimeout."\n");
+			fwrite($file,"host_check_timeout=".$hostchecktimeout."\n");
+			fwrite($file,"notification_timeout=".$notiftimeout."\n");
 			fwrite($file,"event_handler_timeout=30\n");
 			fwrite($file,"ocsp_timeout=5\n");
 			fwrite($file,"perfdata_timeout=5\n");
@@ -188,7 +223,7 @@
 			fwrite($file,"accept_passive_service_checks=1\n");
 			fwrite($file,"execute_host_checks=1\n");
 			fwrite($file,"accept_passive_host_checks=1\n");
-			//fwrite($file,"enable_notifications=".$notifenable."\n");
+			fwrite($file,"enable_notifications=".$notifenable."\n");
 			fwrite($file,"enable_event_handlers=1\n");
 			fwrite($file,"process_performance_data=0\n");
 			fwrite($file,"obsess_over_services=0\n");
@@ -197,13 +232,13 @@
 			fwrite($file,"passive_host_checks_are_soft=0\n");
 			fwrite($file,"check_for_orphaned_services=1\n");
 			fwrite($file,"check_for_orphaned_hosts=1\n");
-			//fwrite($file,"service_check_timeout_state=".$timeoutstate."\n");
+			///write($file,"service_check_timeout_state=".$timeoutstate."\n");
 			fwrite($file,"check_service_freshness=1\n");
 			fwrite($file,"service_freshness_check_interval=60\n");
 			fwrite($file,"check_host_freshness=1\n");
 			fwrite($file,"host_freshness_check_interval=60\n");
 			fwrite($file,"additional_freshness_latency=15\n");
-			//fwrite($file,"enable_flap_detection=".$flapenable."\n");
+			fwrite($file,"enable_flap_detection=".$flapenable."\n");
 			fwrite($file,"low_service_flap_threshold=5.0\n");
 			fwrite($file,"high_service_flag_threshold=20.0\n");
 			fwrite($file,"low_host_flap_threshold=5.0\n");
@@ -220,7 +255,7 @@
 			fwrite($file,"illegal_macro_output_chars=`~$&|'\"<>\n");
 			fwrite($file,"use_regexp_matching=0\n");
 			fwrite($file,"use_true_regexp_matching=0\n");
-			//fwrite($file,"admin_email=".$adminemail."\n");
+			fwrite($file,"admin_email=".$adminemail."\n");
 			//fwrite($file,"admin_pager=".$adminemail."\n");
 			fwrite($file,"daemon_dumps_core=0\n");
 			fwrite($file,"use_large_installation_tweaks=0\n");
