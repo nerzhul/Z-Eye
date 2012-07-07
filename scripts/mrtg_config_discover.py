40109f0d64fc1c1858f33648667b327f5cae59c3
Ner'zhul <loic.blot@frostsapphirestudios.com>
2012-07-07 18:34:43 +0200

    mrtg_config_discover is now fully implemented in python

--- scripts/mrtg_config_discover.py
+++ scripts/mrtg_config_discover.py
@@ -1,42 +1,77 @@
 #! python
 # -*- coding: utf-8 -*-
 
-import _mysql
-from pyPgSQL import libpq
+import MySQLdb as mdb
+from pyPgSQL import PgSQL
 import datetime
 import sys
+import thread
+import commands
+import os
+import re
+import time
+
+threadCounter = 0
+
+def fetchMRTGInfos(ip,devname,devcom):
+	global threadCounter
+	try:
+		threadCounter = threadCounter + 1
+		cmd = "cfgmaker %s@%s" % (devcom,ip)
+                pipe = os.popen('{ ' + cmd + '; }', 'r')
+                text = pipe.read()
+                pipe.close()
+                text = re.sub("\/var\/www\/mrtg","/var/www/datas/rrd",text)
+                cfgfile = open("/var/www/datas/mrtg-config/mrtg-%s.cfg" % devname,"w")
+                cfgfile.writelines(text)
+                cfgfile.close()
+		threadCounter = threadCounter - 1
+	except Exception, e:
+		print "[FATAL] %s" % e
+		threadCounter = threadCounter - 1
 
 mysqlCon = None
 pgsqlCon = None
+
+defaultSNMPRO = 'public'
 
 mysqlHost = 'localhost'
 mysqlUser = 'root'
 mysqlPwd = 'root'
 mysqlDb = 'fssmanager'
 
-pgsqlHost = 'localhost'
+pgsqlHost = '127.0.0.1'
 pgsqlUser = 'netdisco'
 pgsqlPwd = 'netdisco'
 pgsqlDb = 'netdisco'
 
 now = datetime.datetime.now()
 print "[Z-Eye][mrtg-config-discover] Start at: %s" % now.strftime("%Y-%m-%d %H:%M")
-
 try:
 
-	mysqlCon = _mysql.connect(mysqlHost,mysqlUser,mysqlPwd,mysqlDb);
+	mysqlCon = mdb.connect(mysqlHost,mysqlUser,mysqlPwd,mysqlDb);
 	try:
-		pgsqlCon = libpq.PQconnectdb("host=%s dbname=%s user=%s password=%s",pgsqlHost,pgsqlDb,pgsqlUser,pgsqlPwd)
+		pgsqlCon = PgSQL.connect(host=pgsqlHost,user=pgsqlUser,password=pgsqlPwd,database=pgsqlDb)
 		pgcursor = pgsqlCon.cursor()
-		pgcursor.execute("SELECT ip,name FROM device")
+		pgcursor.execute("SELECT ip,name FROM device ORDER BY ip")
 		try:
 			pgres = pgcursor.fetchall()
-			for i in res:
-				print "0: %s 1: %s" % (res[i][0],res[i][1])
+			for idx in pgres:
+				mycur = mysqlCon.cursor()
+				mycur.execute("SELECT snmpro FROM fss_snmp_cache where device = '%s'" % idx[1])
+				myres = mycur.fetchone()
+
+				devip = idx[0]
+				devname = idx[1]
+				if myres:
+					devcom = myres[0]
+				else:
+					devcom = defaultSNMPRO
+				thread.start_new_thread(fetchMRTGInfos,(devip,devname,devcom))
 		except StandardError, e:
-			print "[Z-Eye][mrtg-config-discover] Pgsql Error %s" % e
+			print "[Z-Eye][mrtg-config-discover] Fatal Error: %s" % e
 		
-	except libpq.Error, e:
+	except PgSQL.Error, e:
 		print "[Z-Eye][mrtg-config-discover] Pgsql Error %s" % e
 		sys.exit(1);	
 	
@@ -49,8 +84,13 @@ except _mysql.Error, e:
 finally:
 
 	if mysqlCon:
-		con.close()
-	
+		mysqlCon.close()
+
+	while threadCounter > 0:
+		print "Thread Number %d" % threadCounter
+		time.sleep(1)
+
 	totaltime = datetime.datetime.now() - now
 	now = datetime.datetime.now()
 	print "[Z-Eye][mrtg-config-discover] End at: %s (Total time %s)" % (now.strftime("%Y-%m-%d %H:%M"), totaltime)
+
