5a25b3595c1048735bced4408cd0b76dbe095f0a
Ner'zhul <loic.blot@frostsapphirestudios.com>
2012-07-14 14:34:16 +0200

    Remove useless Category class

--- modules/LocalInterface.class.php
+++ modules/LocalInterface.class.php
@@ -18,7 +18,6 @@
 	* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 	*/
 	
-	require_once(dirname(__FILE__)."/Category.class.php");
 	require_once(dirname(__FILE__)."/../lib/FSS/User.FS.class.php");
 	require_once(dirname(__FILE__)."/../lib/FSS/HTTPLink.FS.class.php");
 	
