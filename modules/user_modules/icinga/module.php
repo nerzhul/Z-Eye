46b1847691b4e91190117ca561b63f60b319c7ca
Loïc BLOT <loic.blot@frostsapphirestudios.com>
2012-11-02 20:44:47 +0100

    Prepare interface for icinga

--- modules/user_modules/icinga/module.php
+++ modules/user_modules/icinga/module.php
@@ -70,6 +70,37 @@
 		private function showHostsTab() {
 			$output = "";
 			
+			$formoutput = "<table><tr><th>".$this->loc->s("Option")."</th><th>".$this->loc->s("Value")."</th></tr>";
+			$formoutput .= FS::$iMgr->addIndexedCheckLine($this->loc->s("is-template"),"istemplate",true);
+			//$formoutput .= template list
+			$formoutput .= FS::$iMgr->addIndexedLine($this->loc->s("Name"),"name","");
+			
+			// Checks
+			//$formoutput .= checkcommand
+			//$formoutput .= checkperiod
+			$formoutput .= FS::$iMgr->addIndexedNumericLine($this->loc->s("check-interval"),"checkintval",3);
+			$formoutput .= FS::$iMgr->addIndexedNumericLine($this->loc->s("retry-check-interval"),"retcheckintval",1);
+			$formoutput .= FS::$iMgr->addIndexedNumericLine($this->loc->s("max-check"),"maxcheck",10);
+			
+			// Global
+			$formoutput .= FS::$iMgr->addIndexedCheckLine($this->loc->s("eventhdl-en"),"eventhdlen",true);
+			$formoutput .= FS::$iMgr->addIndexedCheckLine($this->loc->s("flap-en"),"flapen",true);
+			$formoutput .= FS::$iMgr->addIndexedCheckLine($this->loc->s("failpredict-en"),"failpreden",true);
+			$formoutput .= FS::$iMgr->addIndexedCheckLine($this->loc->s("perfdata"),"perfdata",true);
+			$formoutput .= FS::$iMgr->addIndexedCheckLine($this->loc->s("retainstatus"),"retstatus",true);
+			$formoutput .= FS::$iMgr->addIndexedCheckLine($this->loc->s("retainnonstatus"),"retnonstatus",true);
+			
+			// Notifications
+			$formoutput .= FS::$iMgr->addIndexedCheckLine($this->loc->s("notif-en"),"notifen",true);
+			// $formoutput .= notifperiod
+			$formoutput .= FS::$iMgr->addIndexedNumericLine($this->loc->s("notif-interval"),"notifintval",0);
+			// $formoutput .= notifoptions
+			// $formoutput .= contactgroups
+			$formoutput .= FS::$iMgr->addTableSubmit("",$this->loc->s("Add"));
+			$formoutput .= "</table>";
+			
+			$output .= FS::$iMgr->opendiv($formoutput,$this->loc->s("new-host"));
+			
 			return $output;
 		}
 		
@@ -82,17 +113,84 @@
 		private function showServicesTab() {
 			$output = "";
 			
+			$formoutput = "<table><tr><th>".$this->loc->s("Option")."</th><th>".$this->loc->s("Value")."</th></tr>";
+			$formoutput .= FS::$iMgr->addIndexedCheckLine($this->loc->s("is-template"),"istemplate",true);
+			//$formoutput .= template list
+			
+			// Global
+			$formoutput .= FS::$iMgr->addIndexedLine($this->loc->s("Name"),"name","");
+			
+			$formoutput .= FS::$iMgr->addIndexedCheckLine($this->loc->s("active-check-en"),"actcheck",true);
+			$formoutput .= FS::$iMgr->addIndexedCheckLine($this->loc->s("passive-check-en"),"pascheck",true);
+			$formoutput .= FS::$iMgr->addIndexedCheckLine($this->loc->s("parallel-check"),"parcheck",true);
+			$formoutput .= FS::$iMgr->addIndexedCheckLine($this->loc->s("obs-over-srv"),"obsess",true);
+			$formoutput .= FS::$iMgr->addIndexedCheckLine($this->loc->s("check-freshness"),"freshness",false);
+			$formoutput .= FS::$iMgr->addIndexedCheckLine($this->loc->s("notif-en"),"notifen",true);
+			$formoutput .= FS::$iMgr->addIndexedCheckLine($this->loc->s("eventhdl-en"),"eventhdlen",true);
+			$formoutput .= FS::$iMgr->addIndexedCheckLine($this->loc->s("flap-en"),"flapen",true);
+			$formoutput .= FS::$iMgr->addIndexedCheckLine($this->loc->s("failpredict-en"),"failpreden",true);
+			$formoutput .= FS::$iMgr->addIndexedCheckLine($this->loc->s("perfdata"),"perfdata",true);
+			$formoutput .= FS::$iMgr->addIndexedCheckLine($this->loc->s("retainstatus"),"retstatus",true);
+			$formoutput .= FS::$iMgr->addIndexedCheckLine($this->loc->s("retainnonstatus"),"retnonstatus",true);
+			
+			// Checks
+			//$formoutput .= checkperiod
+			$formoutput .= FS::$iMgr->addIndexedNumericLine($this->loc->s("check-interval"),"checkintval",3);
+			$formoutput .= FS::$iMgr->addIndexedNumericLine($this->loc->s("retry-check-interval"),"retcheckintval",1);
+			$formoutput .= FS::$iMgr->addIndexedNumericLine($this->loc->s("max-check"),"maxcheck",10);
+			
+			// Notifications
+			// $formoutput .= notifperiod
+			// $formoutput .= notifoptions
+			$formoutput .= FS::$iMgr->addIndexedNumericLine($this->loc->s("notif-interval"),"notifintval",0);
+			// $formoutput .= contactgroups
+			$formoutput .= FS::$iMgr->addTableSubmit("",$this->loc->s("Add"));
+			$formoutput .= "</table>";
+			
+			$output .= FS::$iMgr->opendiv($formoutput,$this->loc->s("new-service"));
+			
 			return $output;
 		}
 		
 		private function showTimeperiodsTab() {
 			$output = "";
 			
+			$formoutput = "<table><tr><th>".$this->loc->s("Option")."</th><th>".$this->loc->s("Value")."</th></tr>";
+			$formoutput .= FS::$iMgr->addIndexedCheckLine($this->loc->s("is-template"),"istemplate",true);
+			//$formoutput .= template list
+			$formoutput .= FS::$iMgr->addIndexedLine($this->loc->s("Name"),"name","");
+			$formoutput .= FS::$iMgr->addIndexedLine($this->loc->s("Alias"),"alias","");
+			$formoutput .= "<tr><td>".$this->loc->s("Monday")."</td><td>".FS::$iMgr->hourlist("mh","mm")."</td></tr>";
+			$formoutput .= "<tr><td>".$this->loc->s("Tuesday")."</td><td>".FS::$iMgr->hourlist("tuh","tum")."</td></tr>";
+			$formoutput .= "<tr><td>".$this->loc->s("Wednesday")."</td><td>".FS::$iMgr->hourlist("wh","wm")."</td></tr>";
+			$formoutput .= "<tr><td>".$this->loc->s("Thursday")."</td><td>".FS::$iMgr->hourlist("thh","thm")."</td></tr>";
+			$formoutput .= "<tr><td>".$this->loc->s("Friday")."</td><td>".FS::$iMgr->hourlist("fh","fm")."</td></tr>";
+			$formoutput .= "<tr><td>".$this->loc->s("Saturday")."</td><td>".FS::$iMgr->hourlist("sah","sam")."</td></tr>";
+			$formoutput .= "<tr><td>".$this->loc->s("Sunday")."</td><td>".FS::$iMgr->hourlist("suh","sum")."</td></tr>";
+			$formoutput .= FS::$iMgr->addTableSubmit("",$this->loc->s("Add"));
+			$formoutput .= "</table>";
+			
+			$output .= FS::$iMgr->opendiv($formoutput,$this->loc->s("new-timeperiod"));
 			return $output;
 		}
 		
 		private function showContactsTab() {
 			$output = "";
+			
+			$formoutput = "<table><tr><th>".$this->loc->s("Option")."</th><th>".$this->loc->s("Value")."</th></tr>";
+			$formoutput .= FS::$iMgr->addIndexedCheckLine($this->loc->s("is-template"),"istemplate",true);
+			//$formoutput .= template list
+			$formoutput .= FS::$iMgr->addIndexedLine($this->loc->s("Name"),"name","");
+			//$formoutput .= srvnotifperiod
+			//$formoutput .= srvnotifoptions
+			//$formoutput .= srvnotifcmd
+			//$formoutput .= hostnotifperiod
+			//$formoutput .= hostnotifoptions
+			//$formoutput .= hostnotifcmd
+			$formoutput .= FS::$iMgr->addTableSubmit("",$this->loc->s("Add"));
+			$formoutput .= "</table>";
+			
+			$output .= FS::$iMgr->opendiv($formoutput,$this->loc->s("new-contact"));
 			
 			return $output;
 		}
