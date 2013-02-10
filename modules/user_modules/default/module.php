3f76880f94d855bcfac694fe110ce27ff337a5ec
Loïc Blot (@U-Exp) <loic.blot@unix-experience.fr>
2013-02-10 10:51:10 +0100

    Speed reporting now uses HTTPS since recent distribution update

--- modules/user_modules/default/module.php
+++ modules/user_modules/default/module.php
@@ -62,7 +62,7 @@
 
 		private function showIcingaReporting() {
 			// report nagios
-		        $content = file_get_contents("http://127.0.0.1/cgi-bin/icinga/status.cgi?limit=0&start=1");
+		        $content = file_get_contents("https://127.0.0.1/cgi-bin/icinga/status.cgi?limit=0&start=1");
         		preg_match_all("/<table [^>]*status[^>]*>.*<\/table>/si", $content, $body);
 			if(isset($body[0][0])) {
 			        $body = $body[0][0];
@@ -97,7 +97,7 @@
 				$this->totalicinga = count($totalservices[0])-1;
 
 				// report nagios
-                	        $content = file_get_contents("http://localhost/cgi-bin/icinga/status.cgi?limit=0&servicestatustypes=28");
+                	        $content = file_get_contents("https://localhost/cgi-bin/icinga/status.cgi?limit=0&servicestatustypes=28");
                         	preg_match_all("/<table [^>]*status[^>]*>.*<\/table>/si", $content, $body);
 	                        $body = $body[0][0];
 
