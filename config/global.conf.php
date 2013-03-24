57938d7fba5622b36a57a12a80d4c6679d592cbd
Loïc Blot (@U-Exp) <loic.blot@unix-experience.fr>
2013-03-24 12:40:17 +0100

    Add tag current to this version

--- config/global.conf.php
+++ config/global.conf.php
@@ -2,7 +2,7 @@
 	class Config {
 		private static $lang = "FR-fr";
 		private static $stylesheet = "fss1.css";
-		private static $websiteName = "Z-Eye 1.0";
+		private static $websiteName = "Z-Eye 1.0 (-current)";
 		private static $OS = "FreeBSD";
 		/* 0 (no crypt, not recommended
 		1: sha1
