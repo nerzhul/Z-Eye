056d32ae4b3c3415e9261a5c7d6693e02a6d790a
Loïc BLOT (@CNRS) <loic.blot@institutoptique.fr>
2013-04-28 18:29:27 +0200

    It's now Z-Eye 1.1 -current

--- config/global.conf.php
+++ config/global.conf.php
@@ -2,7 +2,7 @@
 	class Config {
 		private static $lang = "FR-fr";
 		private static $stylesheet = "fss1.css";
-		private static $websiteName = "Z-Eye 1.1 RC (-current)";
+		private static $websiteName = "Z-Eye 1.1 (-current)";
 		private static $OS = "FreeBSD";
 		/* 0 (no crypt, not recommended
 		1: sha1
