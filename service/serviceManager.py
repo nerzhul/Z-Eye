8dbe22863fb25048c7db69f1ab73a25d4269de40
Loïc Blot (@U-Exp) <loic.blot@unix-experience.fr>
2013-04-30 21:23:43 +0200

    Z-Eye daemon: preparation for Z-Eye daemon service

---
+++ service/serviceManager.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+"""
+* Copyright (C) 2010-2012 Loïc BLOT, CNRS <http://www.unix-experience.fr/>
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+"""
+
+import os,re,time,threading
+
+class ZEyeServiceMgr(threading.Thread):
+	sleepingTimer = 0
+	def __init__(self):
+		self.sleepingTimer = 15
+		threading.Thread.__init__(self)
+
+	def run(self):
+		while True:
+			self.restartIcinga()
+			self.restartSnort()
+			time.sleep(self.sleepingTimer)
+
+	def restartIcinga(self):
+		if os.path.exists("/tmp/icinga_restart"):
+			icingaFile = open("/tmp/icinga_restart", 'r')
+			if icingaFile:
+				rf = icingaFile.read()
+				rf = re.sub("\n","",rf)
+				if rf == "1":
+					cmd = "service icinga restart"
+					pipe = os.popen(cmd, 'r')
+					text = pipe.read()
+					pipe.close()
+					os.remove("/tmp/icinga_restart")
+				icingaFile.close()
+			
+	def restartSnort(self):
+		if os.path.exists("/tmp/snort_restart"):
+			snortFile = open("/tmp/snort_restart", 'r')
+			if snortFile:
+				if snortFile.read() == "1":
+					cmd = "service snort restart"
+					pipe = os.popen(cmd, 'r')
+					text = pipe.read()
+					pipe.close()
+					os.remove("/tmp/snort_restart")
+				snortFile.close()
---
+++ service/z-eye.py
@@ -0,0 +1,55 @@
+#! /usr/local/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+* Copyright (C) 2010-2012 Loïc BLOT, CNRS <http://www.unix-experience.fr/>
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+"""
+
+import sys,time
+
+import Daemon
+import serviceManager
+
+class ZEyeDaemon(Daemon.Daemon):
+	def run(self):
+		serviceManager.ZEyeServiceMgr().start()	
+		while True:
+			time.sleep(1)
+
+def usage():
+	print "usage: %s start|stop|restart" % sys.argv[0]
+
+if __name__ == "__main__":
+        daemon = ZEyeDaemon("/var/run/z-eye.pid")
+        if len(sys.argv) == 2:
+                if '-daemon' == sys.argv[1]:
+                        daemon.start()
+			print "Z-Eye daemon started"
+                elif 'stop' == sys.argv[1]:
+			print "Stopping Z-Eye daemon..."
+                        daemon.stop()
+                elif 'restart' == sys.argv[1]:
+                        daemon.restart()
+			print "Restarting Z-Eye daemon"
+                else:
+			print "Unknown arg"
+			usage()
+                        sys.exit(1)
+                sys.exit(0)
+        else:
+		usage()	
+                sys.exit(1)
