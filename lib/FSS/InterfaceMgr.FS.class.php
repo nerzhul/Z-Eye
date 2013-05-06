852f89480456b880e7a3ff99ea7e85f9db6f6150
Loïc Blot (@U-Exp) <loic.blot@unix-experience.fr>
2013-05-06 19:36:13 +0200

    Cleanup

--- lib/FSS/InterfaceMgr.FS.class.php
+++ lib/FSS/InterfaceMgr.FS.class.php
@@ -584,16 +584,14 @@
 			return ($count > 0 ? $output : "");
 		}
 
+		/*
+		* lnkadd option contain get arguments in HTML form for AJAX calls
+		*/
 		public function opendiv($callid,$text1,$options=array()) {
 			$divname = uniqid();
 			$aname = uniqid();
 
-			$content = "";
-                        /*$output = "<div id=\"".$divname."\" style=\"display:none; margin-bottom: 15px;\">".$content.$this->renderJS()."</div>";*/
-			$output = "";
-
-			$this->setJSBuffer(0);
-			$output .= "<a id=\"".$aname."\" href=\"#\" onclick=\"formPopup('".$this->cur_module->getModuleId()."','".$callid."','".
+			$output = "<a id=\"".$aname."\" href=\"#\" onclick=\"formPopup('".$this->cur_module->getModuleId()."','".$callid."','".
 				(isset($options["lnkadd"]) ? $options["lnkadd"] : "")."'";
 			
 			if(isset($options["width"]) && $options["width"] > 0)
