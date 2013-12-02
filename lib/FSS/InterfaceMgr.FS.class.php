726900d8ac4056e05a39422bbac89cc9b9280529
Loïc Blot (@CNRS) <loic.blot@institutoptique.fr>
2013-12-02 17:01:41 +0100

    JS: fix a history bug

--- lib/FSS/InterfaceMgr.FS.class.php
+++ lib/FSS/InterfaceMgr.FS.class.php
@@ -821,7 +821,7 @@
 		}
 		
 		public function setURL($url) {
-			$this->js(sprintf("window.history.pushState(document.documentElement.outerHTML, document.title, '/index.php?mod=%s%s');", 
+			$this->js(sprintf("addHistoryState(document.title, '/index.php?mod=%s%s');", 
 				$this->cur_module->getModuleId(),
 				strlen($url) > 0 ? "&".$url : ""));
 		}
