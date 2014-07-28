af21f77c9e01f711a9c13283c21cb1016dda59d3
Loïc Blot (@U-Exp) <loic.blot@unix-experience.fr>
2014-07-28 08:20:10 +0200

    Z-Eye/Django: add an API to get locales

--- service/WebApp/Z_Eye/urls.py
+++ service/WebApp/Z_Eye/urls.py
@@ -5,10 +5,12 @@ admin.autodiscover()
 
 import views
 
+import InterfaceManager
 import engine.snmpmgmt.Forms
 
 urlpatterns = patterns('',
     #url(r'^admin/', include(admin.site.urls)),
     url(r'^templates/footer', views.Footer.as_view(), name='footer'),
     url(r'^snmpmgmt/forms/community', engine.snmpmgmt.Forms.showCommunity),
+    url(r'^locale/get', InterfaceManager),
 )
