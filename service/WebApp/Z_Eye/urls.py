451dc60c98508c0689071ee869c545821d95593a
Loïc Blot (@U-Exp) <loic.blot@unix-experience.fr>
2014-07-28 08:21:12 +0200

    Z-Eye/Django: fix url

--- service/WebApp/Z_Eye/urls.py
+++ service/WebApp/Z_Eye/urls.py
@@ -12,5 +12,5 @@ urlpatterns = patterns('',
     #url(r'^admin/', include(admin.site.urls)),
     url(r'^templates/footer', views.Footer.as_view(), name='footer'),
     url(r'^snmpmgmt/forms/community', engine.snmpmgmt.Forms.showCommunity),
-    url(r'^locale/get', InterfaceManager),
+    url(r'^locale/get', InterfaceManager.get_locale),
 )
